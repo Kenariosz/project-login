8c29f39410113cf22c2cc1629b36b8f544428c54
Kiss Dávid <kiss.david@ACG100.local>
2016-11-14 03:05:29 +0100

    Fixed css and js path. Add read.me.

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,8 +8,8 @@
 	<title>Login System</title>
 
 	<!-- Styles -->
-	<link href="/css/app.css" rel="stylesheet" type="text/css">
-	<link href="/css/libs.css" rel="stylesheet" type="text/css">
+	<link href="{{ url('/css/app.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ url('/css/libs.css') }}" rel="stylesheet" type="text/css">
 
 </head>
 <body>
@@ -74,7 +74,7 @@
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-<script src="/js/libs.js"></script>
+<script src="{{ url('/js/libs.js') }}"></script>
 
 @include('helpers.flash')
 
